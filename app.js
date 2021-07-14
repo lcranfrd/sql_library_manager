76e1afe674dbfd320494173d4550a3a7fe21ba2a
L. Bennett Crantford <lcranfrd@comcast.net>
2021-07-14 19:23:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 const { error404, internalError } = require('./routes/errors');
 const routes = require('./routes/index');
 const books = require('./routes/books');
-const {sequelize} = require('./models/index.js') //.sequelize;
+const {sequelize} = require('./models/index.js')
 
 const app = express();
 
@@ -23,7 +23,6 @@ const app = express();
   }
 )();
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -37,22 +36,5 @@ app.use('/', routes);
 app.use('/books', books);
 app.use(error404);
 app.use(internalError);
-
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 module.exports = app;
